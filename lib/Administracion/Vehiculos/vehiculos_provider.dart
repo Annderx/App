adddc17c06b34587f90abe970f423e34695cd78f
Annderx <ander012.p@gmail.com>
2025-03-18 12:46:22 -0500

    vehiculos_provider.dart

--- lib/Administracion/Vehiculos/vehiculos_provider.dart
+++ lib/Administracion/Vehiculos/vehiculos_provider.dart
@@ -1,16 +1,52 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'ClasesVehiculos.dart';
 
 class VehiculoProvider with ChangeNotifier {
-  final List<Vehiculo> _vehiculos = [];
+  List<Vehiculo> _vehiculos = [];
 
   List<Vehiculo> get vehiculos => _vehiculos;
 
+  VehiculoProvider() {
+    _cargarVehiculos(); // Carga los vehículos al iniciar
+  }
+
+  Future<void> _cargarVehiculos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? vehiculosJson = prefs.getString('vehiculos');
+
+    if (vehiculosJson != null) {
+      final List<dynamic> decoded = jsonDecode(vehiculosJson);
+      _vehiculos = decoded.map((item) => Vehiculo.fromJson(item)).toList();
+      notifyListeners();
+    }
+  }
+
+  Future<void> _guardarVehiculos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String vehiculosJson = jsonEncode(_vehiculos.map((v) => v.toJson()).toList());
+    await prefs.setString('vehiculos', vehiculosJson);
+  }
+
   void agregarVehiculo(Vehiculo vehiculo) {
     _vehiculos.add(vehiculo);
+    _guardarVehiculos();
     notifyListeners();
   }
 
-  // Métodos adicionales para cargar datos desde una API o base de datos
-  // ...
+  void eliminarVehiculo(String codigoVehiculo) {
+    _vehiculos.removeWhere((v) => v.codigoVehiculo == codigoVehiculo);
+    _guardarVehiculos();
+    notifyListeners();
+  }
+
+  void actualizarVehiculo(Vehiculo vehiculoActualizado) {
+    final index = _vehiculos.indexWhere((v) => v.codigoVehiculo == vehiculoActualizado.codigoVehiculo);
+    if (index != -1) {
+      _vehiculos[index] = vehiculoActualizado;
+      _guardarVehiculos();
+      notifyListeners();
+    }
+  }
 }
