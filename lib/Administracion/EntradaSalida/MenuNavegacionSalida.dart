f9ce5edd7af674fd44739660205b944b1efd36b5
Annderx <ander012.p@gmail.com>
2025-03-18 11:19:10 -0500

    MenuNavegacionSalida.dart

--- lib/Administracion/EntradaSalida/MenuNavegacionSalida.dart
+++ lib/Administracion/EntradaSalida/MenuNavegacionSalida.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Administracion/EntradaSalida/InformeSalida.dart';
+import 'InformeSalida.dart';
 import 'Salida.dart';
 
 class MenuNavegacionSalida extends StatelessWidget {
@@ -10,8 +10,11 @@ class MenuNavegacionSalida extends StatelessWidget {
     return MaterialApp(
       title: 'Salidas App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: const SalidaHomePage(),
     );
   }
@@ -27,37 +30,65 @@ class SalidaHomePage extends StatefulWidget {
 class _SalidaHomePageState extends State<SalidaHomePage> {
   int _selectedIndex = 0;
 
+  final List<Widget> _screens = [
+    const RegistroSalida(),
+    const InformeSalidas(),
+  ];
+
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    setState(() => _selectedIndex = index);
   }
 
   @override
   Widget build(BuildContext context) {
+    bool isWideScreen = MediaQuery.of(context).size.width > 600;
+
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: <Widget>[
-          const RegistroSalida(),
-          const InformeSalidas(),
+      body: Row(
+        children: [
+          if (isWideScreen)
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: _onItemTapped,
+              labelType: NavigationRailLabelType.all,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.remove_circle),
+                  selectedIcon: Icon(Icons.remove_circle, color: Colors.red),
+                  label: Text('Salida'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.list),
+                  selectedIcon: Icon(Icons.list, color: Colors.red),
+                  label: Text('Informe'),
+                ),
+              ],
+            ),
+          Expanded(
+            child: IndexedStack(
+              index: _selectedIndex,
+              children: _screens,
+            ),
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.remove_circle),
-            label: 'Salida',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Informe',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
-      ),
+      bottomNavigationBar: isWideScreen
+          ? null
+          : BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.remove_circle),
+                  label: 'Salida',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.list),
+                  label: 'Informe',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.red,
+              onTap: _onItemTapped,
+            ),
     );
   }
 }
