139f77d09995db3fe19f72a8da469971424252e6
Annderx <ander012.p@gmail.com>
2025-03-18 11:36:16 -0500

    FacturasPagar.dart

--- lib/Administracion/Facturas/FacturasPagar.dart
+++ lib/Administracion/Facturas/FacturasPagar.dart
@@ -18,4 +18,35 @@ class FacturaPagar {
     required this.acuenta2,
     required this.acuenta3,
   });
+
+  /// 🔹 Método para calcular el saldo restante
+  double get saldoPendiente => monto - (acuenta1 + acuenta2 + acuenta3);
+
+  /// 🔹 Convierte el objeto a un `Map` para guardarlo en almacenamiento
+  Map<String, dynamic> toMap() {
+    return {
+      'nroFactura': nroFactura,
+      'proveedor': proveedor,
+      'monto': monto,
+      'formaPago': formaPago,
+      'fecha': fecha.toIso8601String(), // Guardar como String ISO8601
+      'acuenta1': acuenta1,
+      'acuenta2': acuenta2,
+      'acuenta3': acuenta3,
+    };
+  }
+
+  /// 🔹 Crea una instancia de `FacturaPagar` a partir de un `Map`
+  factory FacturaPagar.fromMap(Map<String, dynamic> map) {
+    return FacturaPagar(
+      nroFactura: map['nroFactura'],
+      proveedor: map['proveedor'],
+      monto: map['monto'],
+      formaPago: map['formaPago'],
+      fecha: DateTime.parse(map['fecha']),
+      acuenta1: map['acuenta1'],
+      acuenta2: map['acuenta2'],
+      acuenta3: map['acuenta3'],
+    );
+  }
 }
