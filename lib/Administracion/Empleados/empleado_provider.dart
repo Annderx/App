26880fcb40bc7e91dea89c079263c30e182fded2
Annderx <ander012.p@gmail.com>
2025-03-18 10:56:21 -0500

    empleado_provider.dart

--- lib/Administracion/Empleados/empleado_provider.dart
+++ lib/Administracion/Empleados/empleado_provider.dart
@@ -1,15 +1,72 @@
 import 'package:flutter/material.dart';
-import 'ClasesEmpleados.dart'; // Asegúrate de que la ruta sea correcta
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'ClasesEmpleados.dart';
 
 class EmpleadoProvider extends ChangeNotifier {
-  final List<Empleado> _empleados = [];
+  List<Empleado> _empleados = [];
+  late Database _database;
 
   List<Empleado> get empleados => _empleados;
 
-  void agregarEmpleado(Empleado empleado) {
-    _empleados.add(empleado);
+  EmpleadoProvider() {
+    _initDB();
+  }
+
+  Future<void> _initDB() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'empleados.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          '''CREATE TABLE empleados(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            codigo TEXT,
+            nombre TEXT,
+            area TEXT,
+            tipo TEXT,
+            funcion TEXT,
+            condicion TEXT,
+            telefono TEXT,
+            dni TEXT,
+            direccion TEXT,
+            sueldo REAL,
+            edad INTEGER,
+            fechaNacimiento TEXT
+          )''',
+        );
+      },
+      version: 1,
+    );
+    await obtenerEmpleados();
+  }
+
+  Future<void> agregarEmpleado(Empleado empleado) async {
+    await _database.insert('empleados', empleado.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+    await obtenerEmpleados();
+  }
+
+  Future<void> obtenerEmpleados() async {
+    final List<Map<String, dynamic>> empleadosMap = await _database.query('empleados');
+    _empleados = empleadosMap.map((e) => Empleado.fromMap(e)).toList();
     notifyListeners();
   }
 
-  // Puedes agregar más métodos para modificar o eliminar empleados si es necesario
+  Future<void> actualizarEmpleado(Empleado empleado) async {
+    await _database.update(
+      'empleados',
+      empleado.toMap(),
+      where: 'id = ?',
+      whereArgs: [empleado.id],
+    );
+    await obtenerEmpleados();
+  }
+
+  Future<void> eliminarEmpleado(int id) async {
+    await _database.delete(
+      'empleados',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    await obtenerEmpleados();
+  }
 }
