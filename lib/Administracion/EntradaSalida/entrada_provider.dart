1adf9014eb2f3edb7bb46aa897151a5536153b32
Annderx <ander012.p@gmail.com>
2025-03-18 11:24:32 -0500

    entrada_provider.dart

--- lib/Administracion/EntradaSalida/entrada_provider.dart
+++ lib/Administracion/EntradaSalida/entrada_provider.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
-import 'ClasesEntrada.dart'; // Asegúrate de que la ruta sea correcta
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'ClasesEntrada.dart';
 
 class EntradaProvider extends ChangeNotifier {
   final List<Entrada> _entradas = [];
 
   List<Entrada> get entradas => _entradas;
 
+  EntradaProvider() {
+    _cargarEntradas(); // Cargar entradas guardadas al iniciar
+  }
+
+  Future<void> _cargarEntradas() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? data = prefs.getString('entradas');
+    if (data != null) {
+      final List<dynamic> jsonData = jsonDecode(data);
+      _entradas.clear();
+      _entradas.addAll(jsonData.map((item) => Entrada.fromMap(item)).toList());
+      notifyListeners();
+    }
+  }
+
+  Future<void> _guardarEntradas() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String data = jsonEncode(_entradas.map((e) => e.toMap()).toList());
+    await prefs.setString('entradas', data);
+  }
+
   void agregarEntrada(Entrada entrada) {
     _entradas.add(entrada);
+    _guardarEntradas();
     notifyListeners();
   }
 
-  // Puedes agregar más métodos para modificar o eliminar entradas si es necesario
+  void eliminarEntrada(String codigoEntrada) {
+    _entradas.removeWhere((entrada) => entrada.codigoEntrada == codigoEntrada);
+    _guardarEntradas();
+    notifyListeners();
+  }
+
+  void actualizarEntrada(Entrada entradaActualizada) {
+    final index = _entradas.indexWhere((entrada) => entrada.codigoEntrada == entradaActualizada.codigoEntrada);
+    if (index != -1) {
+      _entradas[index] = entradaActualizada;
+      _guardarEntradas();
+      notifyListeners();
+    }
+  }
+
+  Future<List<Entrada>> obtenerEntradas() async {
+    await _cargarEntradas();
+    return _entradas;
+  }
 }
